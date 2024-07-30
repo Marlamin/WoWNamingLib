9c7a36967f3172a19a30c5be0c5ccf9db05f7b00
Martin Benjamins <ictmartin@gmail.com>
2024-07-30 15:41:21 +0200

    Add expansion folders to music

--- WoWNamingLib/Namers/Music.cs
+++ WoWNamingLib/Namers/Music.cs
@@ -4,14 +4,45 @@ namespace WoWNamingLib.Namers
 {
     public class Music
     {
+        private static string GetFolderName(int fdid)
+        {
+            var addedIn = Namer.GetAddedInPatch(fdid);
+            var folderName = "Unknown";
+            if (addedIn > 0)
+            {
+                if (addedIn >= 130000)
+                {
+                    folderName = "LastTitan";
+                }
+                else if (addedIn >= 120000)
+                {
+                    folderName = "Midnight";
+                }
+                else if (addedIn >= 110000)
+                {
+                    folderName = "WarWithin";
+                }
+                else if (addedIn >= 100000)
+                {
+                    folderName = "Dragonflight";
+                }
+                else if (addedIn >= 90000)
+                {
+                    folderName = "Shadowlands";
+                }
+            }
+
+            return folderName;
+        }
+
         public static void Name()
         {
-            var soundKitDB = Namer.LoadDBC("SoundKitEntry");
+            var soundKitEntryDB = Namer.LoadDBC("SoundKitEntry");
             var soundKitFDIDMap = new Dictionary<uint, List<int>>();
-            foreach (var soundKitEntry in soundKitDB.Values)
+            foreach (var soundKitEntry in soundKitEntryDB.Values)
             {
-                var soundKitID = uint.Parse(soundKitEntry["SoundKitID"].ToString());
-                var soundKitFileDataID = int.Parse(soundKitEntry["FileDataID"].ToString());
+                var soundKitID = uint.Parse(soundKitEntry["SoundKitID"].ToString()!);
+                var soundKitFileDataID = int.Parse(soundKitEntry["FileDataID"].ToString()!);
                 if (!soundKitFDIDMap.ContainsKey(soundKitID))
                 {
                     soundKitFDIDMap.Add(soundKitID, new List<int>() { soundKitFileDataID });
@@ -22,10 +53,12 @@ namespace WoWNamingLib.Namers
                 }
             }
 
+            var doneSoundKits = new List<uint>();
+
             var zoneMusicDB = Namer.LoadDBC("ZoneMusic");
             foreach (var zoneMusicRow in zoneMusicDB.Values)
             {
-                var setName = zoneMusicRow["SetName"].ToString();
+                var setName = zoneMusicRow["SetName"].ToString()!.Trim();
                 for (int i = 0; i < 2; i++)
                 {
                     var soundID = ((uint[])zoneMusicRow["Sounds"])[i];
@@ -33,11 +66,13 @@ namespace WoWNamingLib.Namers
                     {
                         foreach (var soundFDID in soundFDIDs)
                         {
-                            if (Namer.IDToNameLookup.ContainsKey(soundFDID))
+                            if (Namer.IDToNameLookup.ContainsKey(soundFDID) && !Namer.placeholderNames.Contains(soundFDID))
                                 continue;
 
-                            NewFileManager.AddNewFile(soundFDID, "sound/music/unknown/" + setName + "_" + soundFDID + ".mp3");
+                            NewFileManager.AddNewFile(soundFDID, "Sound/Music/" + GetFolderName(soundFDID) + "/" + setName + "_" + soundFDID + ".mp3", Namer.placeholderNames.Contains(soundFDID));
                         }
+
+                        doneSoundKits.Add(soundID);
                     }
                 }
             }
@@ -45,19 +80,43 @@ namespace WoWNamingLib.Namers
             var zoneIntroMusicDB = Namer.LoadDBC("ZoneIntroMusicTable");
             foreach (var zoneIntroMusicRow in zoneIntroMusicDB.Values)
             {
-                var setName = zoneIntroMusicRow["Name"].ToString();
+                var setName = zoneIntroMusicRow["Name"].ToString()!.Trim();
                 var soundID = (uint)zoneIntroMusicRow["SoundID"];
                 if (soundKitFDIDMap.TryGetValue(soundID, out var soundFDIDs))
                 {
                     foreach (var soundFDID in soundFDIDs)
                     {
-                        if (Namer.IDToNameLookup.ContainsKey(soundFDID))
+                        if (Namer.IDToNameLookup.ContainsKey(soundFDID) && !Namer.placeholderNames.Contains(soundFDID))
                             continue;
 
-                        NewFileManager.AddNewFile(soundFDID, "sound/music/unknown/" + setName + "_" + soundFDID + ".mp3");
+                        NewFileManager.AddNewFile(soundFDID, "Sound/Music/" + GetFolderName(soundFDID) + "/" + setName + "_" + soundFDID + ".mp3", Namer.placeholderNames.Contains(soundFDID));
                     }
+
+                    doneSoundKits.Add(soundID);
                 }
             }
+
+            //var soundKitDB = Namer.LoadDBC("SoundKit");
+            //foreach (var soundKitEntry in soundKitDB.Values)
+            //{
+            //    var soundKitID = (int)soundKitEntry["ID"];
+            //    if (doneSoundKits.Contains((uint)soundKitID))
+            //        continue;
+
+            //    if (soundKitEntry["SoundType"].ToString() == "28")
+            //    {
+            //        if (soundKitFDIDMap.TryGetValue((uint)soundKitID, out var soundFDIDs))
+            //        {
+            //            foreach (var soundFDID in soundFDIDs)
+            //            {
+            //                if (Namer.IDToNameLookup.ContainsKey(soundFDID) && !Namer.placeholderNames.Contains(soundFDID))
+            //                    continue;
+
+            //                NewFileManager.AddNewFile(soundFDID, "sound/music/" + GetFolderName(soundFDID) + "/" + "unk_" + soundKitID + "_" + soundFDID + ".mp3", Namer.placeholderNames.Contains(soundFDID));
+            //            }
+            //        }
+            //    }
+            //}
         }
     }
 }
