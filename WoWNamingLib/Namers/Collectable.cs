5c88ec2c101998a6af4c876f75bfecca5b3a6ecf
Martin Benjamins <ictmartin@gmail.com>
2026-01-03 16:10:28 +0100

    Collectable naming rework

--- WoWNamingLib/Namers/Collectable.cs
+++ WoWNamingLib/Namers/Collectable.cs
@@ -6,8 +6,9 @@ namespace WoWNamingLib.Namers
     {
         struct CollectableEntry
         {
-            public bool isModel;
             public uint itemAppearanceID;
+            public List<uint> ModelFDIDs;
+            public List<uint> TextureFDIDs;
             public string color;
             public string description;
             public string filename;
@@ -141,23 +142,25 @@ namespace WoWNamingLib.Namers
             var collectableSourceInfo = Namer.LoadDBC("CollectableSourceInfo");
             foreach (var csiRow in collectableSourceInfo.Values)
             {
-                var itemModifiedAppearanceID = int.Parse(csiRow["ItemModifiedAppearanceID"].ToString());
+                var itemModifiedAppearanceID = int.Parse(csiRow["ItemModifiedAppearanceID"]!.ToString()!);
                 if (!imaMap.TryGetValue(itemModifiedAppearanceID, out var itemModifiedAppearanceRow))
                     continue;
 
-                var itemAppearanceID = int.Parse(itemModifiedAppearanceRow["ItemAppearanceID"].ToString());
+                var itemAppearanceID = int.Parse(itemModifiedAppearanceRow["ItemAppearanceID"]!.ToString()!);
                 if (!iaMap.TryGetValue(itemAppearanceID, out var itemAppearanceRow))
                     continue;
 
-                var itemDisplayInfoID = int.Parse(itemAppearanceRow["ItemDisplayInfoID"].ToString());
+                var itemDisplayInfoID = int.Parse(itemAppearanceRow["ItemDisplayInfoID"]!.ToString()!);
                 if (!idiMap.TryGetValue(itemDisplayInfoID, out var itemDisplayInfoRow))
                     continue;
 
+                var description = csiRow["Description"].ToString()!.Replace("Monster - ", "");
 
-                var description = csiRow["Description"].ToString().Replace("Monster - ", "");
+                if (!description.Contains("Item Appearance"))
+                    continue;
 
                 // Manual overrides for some lines
-                var id = uint.Parse(csiRow["ID"].ToString());
+                var id = uint.Parse(csiRow["ID"]!.ToString()!);
 
                 if (id == 3535)
                     description = "Item Appearance: (9048) - 10.0.0 Weapon Mace2H - MISC_2H_TUSKARFISHINGPOLE_A_01";
@@ -181,12 +184,26 @@ namespace WoWNamingLib.Namers
                     Console.WriteLine("Error parsing: " + description);
                     continue;
                 }
+
+                if (!description.Contains(itemAppearanceID.ToString()))
+                {
+                    Console.WriteLine("ItemAppearanceID mismatch in description: " + description + " vs " + itemAppearanceID);
+                    continue;
+                }
+
+                var filename = splitDescription[2].Trim();
+
+                if (splitDescription[1].Trim().Contains("Armor Cloak"))
+                    filename = "Cape_" + filename;
+
                 var collectableEntry = new CollectableEntry()
                 {
-                    itemAppearanceID = uint.Parse(splitDescription[0].Replace("Item Appearance: (", "").Replace(")", "")),
+                    itemAppearanceID = (uint)itemAppearanceID, // We can not rely on the ItemAppearanceID from the name -- these are somethings wrong/old (e.g. CollectableSourceInfo ID 51909 and 173716	
                     description = splitDescription[1].Trim(),
-                    filename = splitDescription[2].Trim(),
-                    raw = csiRow["Description"].ToString(),
+                    filename = filename,
+                    raw = csiRow["Description"]!.ToString()! + " - WTL IAID: " + itemAppearanceID, // We append the actual ItemAppearanceID to correct for the above issue
+                    ModelFDIDs = [],
+                    TextureFDIDs = []
                 };
 
                 if (splitDescription.Length == 4)
@@ -195,50 +212,41 @@ namespace WoWNamingLib.Namers
                 for (int i = 0; i < 2; i++)
                 {
                     var mrID = ((int[])itemDisplayInfoRow["ModelResourcesID"])[i];
-                    collectableEntry.isModel = true;
-                    if (mfdMap.TryGetValue(mrID, out List<uint> fileDataIDs))
+                    var mmrID = ((int[])itemDisplayInfoRow["ModelMaterialResourcesID"])[i];
+
+                    if (mrID != 0 && mfdMap.TryGetValue(mrID, out List<uint> modelFileDataIDs))
+                        collectableEntry.ModelFDIDs.AddRange(modelFileDataIDs);
+
+                    if (mmrID != 0 && tfdMap.TryGetValue(mmrID, out List<uint> textureFileDataIDs))
+                        collectableEntry.TextureFDIDs.AddRange(textureFileDataIDs);
+                }
+
+                foreach (var modelFDID in collectableEntry.ModelFDIDs)
+                {
+                    if (fdidMap.TryGetValue(modelFDID, out List<CollectableEntry>? collectableEntries))
                     {
-                        foreach (var fileDataID in fileDataIDs)
-                        {
-                            if (fdidMap.ContainsKey(fileDataID))
-                            {
-                                fdidMap[fileDataID].Add(collectableEntry);
-                            }
-                            else
-                            {
-                                fdidMap.Add(fileDataID, new List<CollectableEntry>() { collectableEntry });
-                            }
-                        }
+                        collectableEntries.Add(collectableEntry);
+                    }
+                    else
+                    {
+                        fdidMap.Add(modelFDID, new List<CollectableEntry>() { collectableEntry });
                     }
                 }
 
-                for (int i = 0; i < 2; i++)
+                foreach (var textureFDID in collectableEntry.TextureFDIDs)
                 {
-                    var mmrID = ((int[])itemDisplayInfoRow["ModelMaterialResourcesID"])[i];
-                    collectableEntry.isModel = false;
-                    if (tfdMap.TryGetValue(mmrID, out List<uint> fileDataIDs))
+                    if (fdidMap.TryGetValue(textureFDID, out List<CollectableEntry>? collectableEntries))
                     {
-                        foreach (var fileDataID in fileDataIDs)
-                        {
-                            if (fdidMap.ContainsKey(fileDataID))
-                            {
-                                fdidMap[fileDataID].Add(collectableEntry);
-                            }
-                            else
-                            {
-                                fdidMap.Add(fileDataID, new List<CollectableEntry>() { collectableEntry });
-                            }
-                        }
+                        collectableEntries.Add(collectableEntry);
+                    }
+                    else
+                    {
+                        fdidMap.Add(textureFDID, new List<CollectableEntry>() { collectableEntry });
                     }
                 }
             }
-            // }
-            // catch (Exception e)
-            // {
-            //      Console.WriteLine("Error loading DB2s: " + e.Message);
-            // }
 
-            var output = new List<string>();
+            //var output = new List<string>();
             foreach (var collectableList in fdidMap.OrderBy(x => x.Key))
             {
                 var filedataid = collectableList.Key;
@@ -246,35 +254,33 @@ namespace WoWNamingLib.Namers
                 if (Namer.IDToNameLookup.TryGetValue((int)filedataid, out var name))
                 {
                     currentName = Path.GetFileNameWithoutExtension(name);
-                    output.Add(filedataid + ";" + name);
+                    //output.Add(filedataid + ";" + name);
                 }
                 else
                 {
                     currentName = filedataid.ToString();
-                    output.Add(filedataid + ";unnamed/" + filedataid);
+                    //output.Add(filedataid + ";unnamed/" + filedataid);
                 }
 
                 var uniqueList = collectableList.Value.DistinctBy(x => x.raw);
 
-                if (uniqueList.All(x => x.filename.ToLower() == uniqueList.First().filename.ToLower()))
+                if (uniqueList.All(x => x.filename.Equals(uniqueList.First().filename, StringComparison.OrdinalIgnoreCase)))
                 {
                     var first = uniqueList.First();
                     var filenameMatches = false;
 
-                    if (first.filename.ToLower() == currentName.ToLower())
+                    if (first.filename.Equals(currentName, StringComparison.OrdinalIgnoreCase))
                         filenameMatches = true;
 
-                    if (!first.isModel && first.color != null && first.filename.ToLower() == currentName.ToLower() + first.color.ToLower())
+                    if (first.color != null && first.filename.Equals(currentName + first.color, StringComparison.OrdinalIgnoreCase))
                         filenameMatches = true;
 
-                    if (first.isModel && itemModelSuffixes.ContainsKey(filedataid) && first.filename.ToLower() == currentName.ToLower() + itemModelSuffixes[filedataid].ToLower())
+                    string? itemModelSuffix = null;
+                    if (itemModelSuffixes.TryGetValue(filedataid, out itemModelSuffix) && first.filename.Equals(currentName + itemModelSuffix, StringComparison.OrdinalIgnoreCase))
                         filenameMatches = true;
 
-                    if (filenameMatches)
-                        output.Add("\t \u2705 Filename matches");
-                    else
+                    if (!filenameMatches)
                     {
-                        output.Add("\t \u274C Filename does not match");
                         var folder = "";
 
                         switch (first.filename.Split("_")[0].ToLower().Trim())
@@ -342,61 +348,64 @@ namespace WoWNamingLib.Namers
                                 folder = "item/objectcomponents/ammo";
                                 break;
                             default:
-                                Console.WriteLine("Unknown prefix: " + first.filename.Split("_")[0].ToLower());
+                                Console.WriteLine("Unknown prefix: " + first.filename.Split("_")[0].ToLower() + " (" + first.filename + ")");
                                 break;
                         }
-
 
                         if (folder == "")
                             continue;
 
-                        if (!first.isModel)
+                        if (first.TextureFDIDs.Contains(filedataid))
                         {
                             if (currentName.Contains(filedataid.ToString()))
                             {
                                 if (first.color != null)
                                 {
-                                    if (Namer.IDToNameLookup.ContainsKey((int)filedataid) && Namer.placeholderNames.Contains((int)filedataid))
+                                    var firstColorClean = first.color.Replace(" ", "").Replace("(", "").Replace(")", "");
+
+                                    if (first.ModelFDIDs.Count > 0 && Namer.IDToNameLookup.TryGetValue((int)first.ModelFDIDs.First(), out var modelFileName))
                                     {
-                                        //NewFileManager.AddNewFile(filedataid, folder + "/" + currentName.Replace(filedataid.ToString(), first.color.Replace(" ", "").Replace("(", "").Replace(")", "") + ".blp"), true, true);
+                                        if (itemModelSuffixes.TryGetValue(first.ModelFDIDs.First(), out var ogItemModelSuffix) && modelFileName.Contains(ogItemModelSuffix))
+                                            modelFileName = modelFileName.Replace(ogItemModelSuffix, "");
+
+                                        var supposedTextureFilename = modelFileName.Replace(".m2", "_" + firstColorClean + ".blp");
+                                        NewFileManager.AddNewFile(filedataid, supposedTextureFilename, true, true);
                                     }
-                                    else if(Namer.IDToNameLookup.ContainsKey((int)filedataid))
+                                    else
                                     {
-                                        NewFileManager.AddNewFile(filedataid, Namer.IDToNameLookup[(int)filedataid].Replace(filedataid.ToString(), first.color.Replace(" ", "").Replace("(", "").Replace(")", "")), true, true);
+                                        // No model, likely a cape texture or something?
+                                        NewFileManager.AddNewFile(filedataid, folder + "/" + first.filename.Replace(" ", "") + "_" + firstColorClean + ".blp", true, true);
                                     }
                                 }
-
-                                //else
-                                //    NewFileManager.AddNewFile(filedataid, folder + "/" + first.filename.Replace(" ", "") + ".blp", true);
                             }
                         }
                         else
                         {
                             if (Namer.placeholderNames.Contains((int)filedataid))
                             {
-                                if (itemModelSuffixes.ContainsKey(filedataid))
-                                    NewFileManager.AddNewFile(filedataid, folder + "/" + first.filename.Replace(" ", "") + itemModelSuffixes[filedataid] + ".m2", true, true);
+                                if (itemModelSuffix != null)
+                                    NewFileManager.AddNewFile(filedataid, folder + "/" + first.filename.Replace(" ", "") + itemModelSuffix + ".m2", true, true);
                                 else
                                     NewFileManager.AddNewFile(filedataid, folder + "/" + first.filename.Replace(" ", "") + ".m2", true, true);
                             }
                         }
                     }
                 }
-
+                else
+                {
+                    Console.WriteLine("Multiple filenames (" + uniqueList.Count() + ") for FDID " + filedataid + " current name: " + currentName);
+                    foreach (var entry in uniqueList)
+                    {
+                        Console.WriteLine("\t" + entry.itemAppearanceID + ", Description: " + entry.description + ", Filename: " + entry.filename);
+                    }
+                }
                 //foreach (var collectable in uniqueList)
                 //{
                 //    var outputLine = "";
 
-                //    if (collectable.isModel)
-                //    {
-                //        outputLine += "\t ItemAppearanceID: " + collectable.itemAppearanceID + ", Description: " + collectable.description + ", Filename: " + collectable.filename;
-                //    }
-                //    else
-                //    {
-                //        outputLine += "\t ItemAppearanceID: " + collectable.itemAppearanceID + ", Description: " + collectable.description + ", Filename: " + collectable.filename;
-                //        if (collectable.color != null)
-                //            outputLine += ", Color: " + collectable.color;
-                //    }
+                //    outputLine += "\t ItemAppearanceID: " + collectable.itemAppearanceID + ", Description: " + collectable.description + ", Filename: " + collectable.filename;
+                //    if (collectable.color != null)
+                //        outputLine += ", Color: " + collectable.color;
 
                 //    output.Add(outputLine);
                 //}
