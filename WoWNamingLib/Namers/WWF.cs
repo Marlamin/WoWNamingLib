b1b6a266d3431ea49f7c0a68d90146060c212dd4
Martin Benjamins <ictmartin@gmail.com>
2026-02-25 01:08:00 +0100

    wwf => pvdata

--- WoWNamingLib/Namers/WWF.cs
+++ WoWNamingLib/Namers/WWF.cs
@@ -7,14 +7,18 @@ namespace WoWNamingLib.Namers
         public static void Name(bool fullRun = false)
         {
             var wxpDB = Namer.LoadDBC("WeatherXParticulate");
+            if (!wxpDB.AvailableColumns.Contains("FileDataID") || !wxpDB.AvailableColumns.Contains("ParentWeatherID"))
+            {
+                Console.WriteLine("WeatherXParticulate is missing required columns, skipping..."); 
+                return;
+            }
+
             foreach (var wxpEntry in wxpDB.Values)
             {
-                var fileDataID = int.Parse(wxpEntry["FileDataID"].ToString());
-                var parentWeatherID = uint.Parse(wxpEntry["ParentWeatherID"].ToString());
+                var fileDataID = int.Parse(wxpEntry["FileDataID"]!.ToString()!);
+                var parentWeatherID = uint.Parse(wxpEntry["ParentWeatherID"]!.ToString()!);
                 if (!Namer.IDToNameLookup.ContainsKey(fileDataID))
-                {
-                    NewFileManager.AddNewFile(fileDataID, "particles/particulates/weather" + parentWeatherID + "_" + fileDataID + ".wwf");
-                }
+                    NewFileManager.AddNewFile(fileDataID, "Environments/ParticulateVolumes/pvdata/unkweather" + parentWeatherID + "_" + fileDataID + ".pvdata");
             }
 
             // todo: parse wwf
